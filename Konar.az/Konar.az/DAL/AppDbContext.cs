f0473c090e05fdb9f4944ff02df039ce0b9bd06a
MusaOglanov <oglanovmusa2001@gmail.com>
2024-03-26 01:27:01 +0400

    .Net6 ile proyekt elave etdim ve Sql ile elaqe qurdum

---
+++ Konar.az/Konar.az/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Konar.az.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Konar.az.DAL
+{
+	public class AppDbContext: DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option)
+        {
+                
+        }
+		public DbSet<Test> Test { get; set; }
+	}
+	
+}
---
+++ Konar.az/Konar.az/Migrations/20240325212310_CreateTestTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Konar.az.Migrations
+{
+    public partial class CreateTestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Test",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Test", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Test");
+        }
+    }
+}
