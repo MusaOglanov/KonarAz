a60f282593b185b68ee4dccebfc96c92008bc015
MusaOglanov <oglanovmusa2001@gmail.com>
2024-10-08 22:10:12 +0400

    Bio tablesinin datalarını Home Səhifəsinə gətirdim

---
+++ Konar.az/Konar.az/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Konar.az.DAL;
+using Konar.az.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Konar.az.ViewComponents
+{
+	public class FooterViewComponent:ViewComponent
+	{
+		private readonly AppDbContext _db;
+		public FooterViewComponent(AppDbContext db)
+		{
+			_db = db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			Bio bio = await _db.Bios.FirstOrDefaultAsync();
+			return View(bio);
+		}
+	}
+}
---
+++ Konar.az/Konar.az/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Konar.az.DAL;
+using Konar.az.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Konar.az.ViewComponents
+{
+	public class HeaderViewComponent:ViewComponent
+	{
+		private readonly AppDbContext _db;
+        public HeaderViewComponent(AppDbContext db)
+        {
+			_db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+		{
+		    Bio bio = await _db.Bios.FirstOrDefaultAsync();
+			return View(bio);
+		}
+	}
+}
